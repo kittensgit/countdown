02aed1f8ca5e57156a7d098ceda40407ef3545e2
Nika <nikaonime@gmail.com>
2023-06-26 14:46:50 +0300

    something wrong

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import CountdownTimer from './Compoments/CountdownTimer/CountdownTimer';
 function App() {
   return (
     <div className="App">
-      <CountdownTimer />
+      <CountdownTimer
+        countdownTimestampMs={16329600000} />
     </div>
   );
 }
+
 
 export default App;
--- src/Compoments/CountdownTimer/CountdownTimer.js
+++ src/Compoments/CountdownTimer/CountdownTimer.js
@@ -1,29 +1,40 @@
-import React, { useState } from 'react'
-import './CountdownTimer.css'
+import {useState, useEffect} from 'react';
+import './CountdownTimer.css';
+import {getRemainingTimeUntilMsTimestamp} from './Utils/CountdownTimerUtils';
 
-const CountdownTimer = () => {
-
-    const defauttRemainingTime = {
-        seconds: '00',
-        minutes: '00',
-        hours: '00',
-        days: '00'
-    }
-
-    const [remainingTime, setRemainingTime] = useState(defauttRemainingTime)
-
-    return (
-        <div className='countdown-timer'>
-            <span>{remainingTime.days}</span>
-            <span>days</span>
-            <span>{remainingTime.hours}</span>
-            <span>hours</span>
-            <span>{remainingTime.minutes}</span>
-            <span>minutes</span>
-            <span>{remainingTime.seconds}</span>
-            <span>seconds</span>
-        </div>
-    )
+const defaultRemainingTime = {
+    seconds: '00',
+    minutes: '00',
+    hours: '00',
+    days: '00'
 }
 
-export default CountdownTimer
+const CountdownTimer = ({countdownTimestampMs}) => {
+    const [remainingTime, setRemainingTime] = useState(defaultRemainingTime);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            updateRemainingTime(countdownTimestampMs);
+        }, 1000);
+        return () => clearInterval(intervalId);
+    },[countdownTimestampMs]);
+
+    function updateRemainingTime(countdown) {
+        setRemainingTime(getRemainingTimeUntilMsTimestamp(countdown));
+    }
+
+    return(
+        <div className="countdown-timer">
+            <span>{remainingTime.days}</span>
+            <span>days</span>
+            <span className="two-numbers">{remainingTime.hours}</span>
+            <span>hours</span>
+            <span className="two-numbers">{remainingTime.minutes}</span>
+            <span>minutes</span>
+            <span className="two-numbers">{remainingTime.seconds}</span>
+            <span>seconds</span>
+        </div>
+    );
+}
+
+export default CountdownTimer;
